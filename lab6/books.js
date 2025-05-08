cbac8dd6be38b0d6923cc3746c8b17cf89b92b81
Александра Денисенко <denisenko.sos@yandex.ru>
2025-05-08 19:13:50 +0700

    книги

---
+++ lab6/books.js
@@ -0,0 +1,106 @@
+const books = [
+  {
+    author: 'Лао-цзы',
+    name: 'Дао дэ цзин. Канон о Дао и дэ',
+    publicationYear: 2022,
+    price: 945,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: 'Карл Саган',
+    name: 'Космос',
+    publicationYear: 2024,
+    price: 2483,
+    bestseller: true,
+    inStock: true
+  },
+  {
+    author: 'Баладжи Касал',
+    name: 'Ошибки разумного инвестора',
+    publicationYear: 2025,
+    price: 901,
+    bestseller: false,
+    inStock: false
+  },
+  {
+    author: "Джон Рональд Руэл Толкин",
+    name: "Властелин колец",
+    publicationYear: 2025,
+    price: 2575,
+    bestseller: true,
+    inStock: true
+  },
+  {
+    author: "Агата Кристи",
+    name: 'Убийство в "Восточном экспрессе"',
+    publicationYear: 2024,
+    price: 689,
+    bestseller: true,
+    inStock: true
+  },
+  {
+    author: "Майк Омер",
+    name: 'Внутри убийцы',
+    publicationYear: 2025,
+    price: 759,
+    bestseller: false,
+    inStock: false
+  },
+  {
+    author: "Саша Сулим",
+    name: "Безлюдное место: Как ловят маньяков в России",
+    publicationYear: 2023,
+    price: 559,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Стивен Кинг",
+    name: "Клатбище домашних жывотных",
+    publicationYear: 2025,
+    price: 671,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Светлана Тюльбашева",
+    name: "Лес",
+    publicationYear: 2024,
+    price: 855,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Маркус Зусак",
+    name: "Я — посланник",
+    publicationYear: 2025,
+    price: 671,
+    bestseller: false,
+    inStock: false
+  },
+  {
+    author: "Сьюзен Коллинз",
+    name: "Голодные игры. И вспыхнет пламя. Сойка - пересмешница",
+    publicationYear: 2025,
+    price: 1471,
+    bestseller: false,
+    inStock: false
+  },
+  {
+    author: "Эдгар По",
+    name: "Убийства на улице Морг",
+    publicationYear: 2025,
+    price: 289,
+    bestseller: false,
+    inStock: true
+  },
+  {
+    author: "Содзи Симада",
+    name: "Токийский Зодиак",
+    publicationYear: 2025,
+    price: 781,
+    bestseller: false,
+    inStock: true
+  },
+];
